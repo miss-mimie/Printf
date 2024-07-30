05fbacf9de8f386b6af3aae067d665564211675b
miss-mimie <mariam.musah55@gmail.com>
2024-07-30 12:31:43 -0700

    Success push

---
+++ _putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _putchar - This is the putchar
+ * 
+ * Returns: 0.
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
---
+++ main.h
@@ -0,0 +1,10 @@
+#ifdef _MAIN_H
+#define _MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+
+#endif
