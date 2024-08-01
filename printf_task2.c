82b7bafdaa8d433016a6dec01c69ffc56bb3544b
Pa-ppy <rayjacks07@gmail.com>
2024-07-31 21:50:46 -0700

    Task 2

---
+++ printf_task2.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * print_bin - Prints unsigned int as binary
+ * @b: The unsigned int to print
+ *
+ * Return: The number of characters printed
+ */
+int print_bin(unsigned int b)
+{
+	int count_variable = 0, i, head_zeros = 1;
+	unsigned int container = 1 << (sizeof(b) * 8 - 1);
+
+	for (i = 0; i < sizeof(b) * 8; i++)
+	{
+		if (b & container)
+		{
+			_putchar('1');
+			count_variable++;
+			head_zeros = 0;
+		}
+		else if (!head_zeros)
+		{
+			_putchar('0');
+			count_variable++;
+		}
+		container >>= 1;
+	}
+
+	if (count_variable == 0)
+	{
+		_putchar('0');
+		count_variable++;
+	}
+
+	return (count_variable);
+}
